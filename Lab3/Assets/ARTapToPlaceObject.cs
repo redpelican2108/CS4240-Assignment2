97cd93721c1bd2410089eb82fbeba50b5f6c6614
abner <abnermtj@live.com.sg>
2023-02-15 21:53:06 +0800

    Add sht

--- Lab3/Assets/ARTapToPlaceObject.cs
+++ Lab3/Assets/ARTapToPlaceObject.cs
@@ -4,51 +4,83 @@ using System.Collections.Generic;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using UnityEngine;
-
+using LightScrollSnap;
+using TMPro;
 
 public class ARTapToPlaceObject : MonoBehaviour
 {
     [SerializeField] private GameObject placementIndicator;
+
     //change the objectToPlace through UI click
-    [SerializeField] private  GameObject objectToPlace;
+
+    // Scroll bar
+    [SerializeField] private GameObject scrollSnap;
+
+    [SerializeField] private GameObject[] objectList;
+
+    [SerializeField] private GameObject DeleteButton;
+
+    private GameObject objectToPlace;
+
     private Pose PlacementPose;
     private ARRaycastManager aRRaycastManager;
     private bool placementPoseIsValid = false;
     private GameObject selectedObject;
+
+    public GameObject DebugText;
+    public GameObject DebugText2;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         aRRaycastManager = FindObjectOfType<ARRaycastManager>();
+        scrollSnap.GetComponent<ScrollSnap>().OnItemSelected.AddListener(OnSelectedItemChanged);
+        OnSelectedItemChanged(null, 0); // Initialize to select the first item
+    }
 
+    private void OnSelectedItemChanged(RectTransform go, int index)
+    {
+        objectToPlace = objectList[index];
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         UpdatePlacementPose();
         UpdatePlacementIndicator();
-        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                var ray = Camera.current.ScreenPointToRay(touch.position);
+                RaycastHit hitInfo;
+                if (Physics.Raycast(ray, out hitInfo))
+                {
+                    selectedObject = hitInfo.collider.gameObject;
+                    DebugText.GetComponent<TextMeshProUGUI>().text = selectedObject.name;
+                }
+            }
+            DebugText2.GetComponent<TextMeshProUGUI>().text = Input.GetTouch(0).position.y.ToString();
+        }
+    }
+
+    public void AddFurniture()
+    {
+        if (placementPoseIsValid)
         {
             PlaceObject();
         }
-        DetectSelect();
-    }
-
-    private void DetectSelect()
-    {
-        var ray = Camera.current.ScreenPointToRay(Input.GetTouch(0).position);
-        RaycastHit hitInfo;
-        if(Physics.Raycast(ray, out hitInfo))
-        {
-            selectedObject = hitInfo.collider.gameObject;
-        }
-
     }
 
     public void DeleteFurniture()
     {
-        Destroy(selectedObject);
-
+        if (selectedObject)
+        {
+            Destroy(selectedObject);
+            DebugText.GetComponent<TextMeshProUGUI>().text = "No Object Selected";
+        }
     }
 
     private void UpdatePlacementPose()
@@ -58,11 +90,10 @@ public class ARTapToPlaceObject : MonoBehaviour
         aRRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
 
         placementPoseIsValid = hits.Count > 0;
-        if(placementPoseIsValid)
+        if (placementPoseIsValid)
         {
             PlacementPose = hits[0].pose;
         }
-
     }
 
     private void UpdatePlacementIndicator()
